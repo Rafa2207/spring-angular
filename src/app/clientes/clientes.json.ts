afbabf3c56c47433e8e8d9c8f8bccd2efd142d50
Rafael Aguilar <rafa2289@gmail.com>
2020-01-06 21:35:11 -0600

    Carga inicial del proyecto de Spring con Angular

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+ {id:1,nombre: 'Rafael', apellido:'Aguilar', email:'rafa2289@gmail.com', createAt:'2019-05-27'},
+ {id:2,nombre: 'Juan', apellido:'Perez', email:'pxd22@gmail.com', createAt:'2019-05-27'},
+ {id:3,nombre: 'Mario', apellido:'Sosa', email:'alsm33@gmail.com', createAt:'2019-05-27'},
+ {id:4,nombre: 'Agripino', apellido:'Delgado', email:'alaj33@gmail.com', createAt:'2019-05-27'},
+ {id:5,nombre: 'Jose', apellido:'Martinez', email:'jma22@gmail.com', createAt:'2019-05-27'},
+ {id:6,nombre: 'Marcos', apellido:'Marroquin', email:'mmaa22@gmail.com', createAt:'2019-05-27'},
+ {id:7,nombre: 'Pedro', apellido:'Lara', email:'jmss98@gmail.com', createAt:'2019-05-27'},
+ {id:8,nombre: 'Manuel', apellido:'Bonilla', email:'mbola1@gmail.com', createAt:'2019-05-27'},
+ {id:9,nombre: 'Samuel', apellido:'Sanchez', email:'rsx23@gmail.com', createAt:'2019-05-27'},
+ {id:10,nombre: 'Saul', apellido:'Hernandez', email:'shmm22@gmail.com', createAt:'2019-05-27'},
+ {id:11,nombre: 'Rodrigo', apellido:'Avila', email:'pp22km@gmail.com', createAt:'2019-05-27'}
+];
